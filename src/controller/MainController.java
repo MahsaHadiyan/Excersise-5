661c46d4967b25911ca44c1f9965260765e38460
mahsahadiyan27 <mahsahadiyan27@gmail.com>
2023-03-05 23:23:06 +0330

    first commit.

---
+++ src/controller/MainController.java
@@ -0,0 +1,26 @@
+package controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Revision History:
+ * Date            Author           Task ID                         Notes
+ * ==========   =================   ==============  ===============================================
+ * 2023.03.05   Mahsa.Hadiyan
+ */
+public class MainController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
